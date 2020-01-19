b088a575c47bcb265ac72987f22b30359641af2a
quoctruong14 <36171996+quoctruong14@users.noreply.github.com>
2020-01-18 18:49:33 -0600

    Update User.java

--- bankingapp/src/main/java/com/buckets/bankingapp/models/User.java
+++ bankingapp/src/main/java/com/buckets/bankingapp/models/User.java
@@ -1,5 +1,52 @@
 package com.buckets.bankingapp.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	private int user_id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	public User() {
+		super();
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", username=" + username + ", password=" + password + "]";
+	}
 
 }

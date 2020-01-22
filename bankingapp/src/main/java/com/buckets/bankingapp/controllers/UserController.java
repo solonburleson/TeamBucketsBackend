35fbb1a1fb320eb6e005e43f2624cb04662e6f47
Quoc Truong <quoctruong@utexas.edu>
2020-01-22 17:05:11 -0600

    added HATEOAS

--- bankingapp/src/main/java/com/buckets/bankingapp/controllers/UserController.java
+++ bankingapp/src/main/java/com/buckets/bankingapp/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.buckets.bankingapp.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.buckets.bankingapp.models.Todo;
 import com.buckets.bankingapp.models.User;
+import com.buckets.bankingapp.repositories.UserRepository;
 import com.buckets.bankingapp.services.TodoService;
 import com.buckets.bankingapp.services.UserService;
 
@@ -26,6 +30,9 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	TodoService todoService;
+	
+	@Autowired
+	UserRepository userRepo;
 
 	@PostMapping("/users")
     public User createUser(@Valid @RequestBody User user) {
@@ -37,16 +44,36 @@ public class UserController {
 		return userService.lisAllUsers();
 	}
 	
-//	@GetMapping("/users/{id}")
-//	public User getUser(@PathVariable("id") Long id) {
-//		
-//		return userService.getUser(id);
-//	}
+	@GetMapping("/users/{id}")
+	public EntityModel<User> getUser(@PathVariable("id") Long id) {
+		
+		Optional<User> user = userRepo.findById(id);
+		if(!user.isPresent()) {
+			System.out.println("Not present");
+		}
+		
+		EntityModel<User> resource = new EntityModel<User>(user.get());
+	    WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		return resource;
+	}
 	
-	@GetMapping("/users/{name}")
+	@GetMapping("/users/name/{name}")
 	public User getUserByName(@PathVariable("name") String name) {
 		return userService.getUserByName(name);
 	}
+		
+		/*
+		Optional<Student> student = studentRepository.findById(id);
+		if (!student.isPresent())
+			throw new StudentNotFoundException("id-" + id);
+		EntityModel<Student> resource = new EntityModel<Student>(student.get());
+	    WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllStudents());
+
+		resource.add(linkTo.withRel("all-students"));
+		return resource;
+		*/
+	
 	
 	@PostMapping("/users/{id}")
 	public Todo plusTodo(Todo todo, @PathVariable("id") Long id) {
